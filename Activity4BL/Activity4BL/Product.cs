03a6636f441f3b08d884c42db5b9dcb1a1e43aa0
Rohitha Koduri <rohithakoduri@gmail.com>
2021-07-12 14:13:24 +0530

    Activity1

---
+++ Activity4BL/Activity4BL/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity4BL
+{
+    public class Product
+    {
+        private Category category;
+        public static int currentProductId;
+        private string description;
+        private double price;
+        private string productId;
+        private string productName;
+        public Category Category { get; set; }
+        public string Description { get; set; }
+        public double Price { get { return price; } set { price = value; } }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+
+        static Product()
+        {
+
+        }
+        public Product()
+        {
+
+        }
+        public Product(string productName, string description, double price, Category category)
+        {
+
+        }
+    }
+}
+  
---
+++ Activity4BL/Activity4BL/Purchase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity4BL
+{
+    public class Purchase
+    {
+        private Customer customer;
+        private DateTime dateOfPurchase;
+        private string paymentType;
+        public static int purchaseCounter;
+        private string purchaseId;
+        private int quantityOrdered;
+        private string shippingAddress;
+        public Customer Customer { get; set; }
+        public DateTime DateOfPurchase { get; set; }
+        public string PaymentType { get; set; }
+        public string PurchaseId { get; set; }
+        public int QuantityOrdered { get; set; }
+        public string ShippingAddress { get; set; }
+
+        static Purchase()
+        {
+
+        }
+        public Purchase()
+        {
+
+        }
+        public Purchase(Customer customer, int quantityOrdered, string shippingAddress, DateTime dateOfPurchase, string paymentType)
+        {
+
+        }
+
+        public double CalculateDiscount()
+        {
+            return 0.05;
+        }
+        public double CalculateBillAmount(params Product[] products)
+        {
+            double totalPrice = 0.0;
+            foreach (Product price in products)
+            {
+                totalPrice += Convert.ToDouble(price);
+            }
+
+            double serviceTax = 0.07 * totalPrice;
+            double discountPercentage = CalculateDiscount();
+            double totalBillAmount = ((totalPrice + serviceTax) * (1 - discountPercentage));
+            return totalBillAmount;
+        }
+        public double CalculateBillAmount(Product product, int quantityRequired)
+        {
+            double totalPrice = product.Price * quantityRequired;
+            double serviceTax = 0.07 * totalPrice;
+            double discountPercentage = CalculateDiscount();
+            double totalBillAmount = ((totalPrice + serviceTax) * (1 - discountPercentage));
+            return totalBillAmount;
+
+        }
+
+    }
+}
